ddb356176794e8902fd9772ccd36aa949c5b98de
MUtkuCoskun <m.utku.coskun@icloud.com>
2025-09-06 12:02:37 +0200

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  // Build sırasında lint/TS hatalarına takılma
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-}
-
-export default nextConfig
