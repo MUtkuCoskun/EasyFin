303840f0704f75147cfb5a28b9f585438e6e1faa
MUtkuCoskun <m.utku.coskun@icloud.com>
2025-09-06 11:48:50 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  // Build sırasında lint/TS hatalarına takılma
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
-};
-export default nextConfig;
+  typescript: { ignoreBuildErrors: true },
+}
+
+export default nextConfig
